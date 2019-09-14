91b8e3290eb4bee9d5c6db20af5e5116d6524553
HomDx <uqgvhom3dxto@bk.ru>
2019-09-14 13:47:26 +0300

    whereis qmake

--- build-from-source.sh
+++ build-from-source.sh
@@ -12,5 +12,5 @@ export    ANDROID_NDK_TOOLCHAIN_PREFIX=arm-linux-androideabi c && \
 export    ANDROID_NDK_TOOLCHAIN_VERSION=4.9 c && \
 export DEBIAN_FRONTEND=noninteractive c && \
 cd /Qt/5.13.1/Src && echo start build && date && LANG=C ./configure -android-arch armeabi-v7a -opensource -confirm-license -release -nomake tests -nomake examples -no-compile-examples -android-sdk /android-sdk-linux -android-ndk /android-ndk-r19c -xplatform android-clang -no-warnings-are-errors --disable-rpath && \
-make && echo end build && date && echo build done && make install && cd /Qt/5.13.1/Src/qtbase/src/tools/androiddeployqt && make && make install &&  echo done1 && date && rm -rf /Qt && date && echo all done ok || echo error build
+make -j2 --no-print-directory && echo end build && date && echo build done && make install && cd /Qt/5.13.1/Src/qtbase/src/tools/androiddeployqt && make && make install &&  echo done1 && date && rm -rf /Qt && date && echo all done ok || echo error build
 
